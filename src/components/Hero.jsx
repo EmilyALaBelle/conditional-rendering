73fbf8020a472e735618bd7177df35850fbe8c7c
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-28 11:20:56 -0400

    Log Out button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import React, { useState, } from "react";
 
 function Hero() {
     const [user, setUser] = useState()
     console.log({ user }) //User is falsey aka undefined
-    const handleLogin = () => {}
+    const handleLogin = () => {
+        setUser({ firstName: 'Emily', email: 'emilylabelle8@gmail.com', admin: true })
+    }
+
     if (!user) { //!user = truthy becaue it is true that it is false
         return <button onClick={handleLogin}>Login</button>
     }
     return (
-        <h1>This is React!</h1>
+        <>
+            <h1>Hello {user.firstName}</h1>
+            <button>Log Out</button>
+        </>
     )
 }
 
