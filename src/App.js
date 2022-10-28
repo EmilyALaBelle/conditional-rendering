ad4eae4b00271b63c974fc857e249ee71bb10bc6
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-28 10:34:19 -0400

    FIRST

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+  const myName = 'Emily'
   return (
     <div className="App">
       <header className="App-header">
-        
+        {myName
+        ? <p>Welome Back</p>
+        :<button>Login</button>
+        }
+        <p>Hello {myName || 'guest '}</p>
       </header>
     </div>
   );
